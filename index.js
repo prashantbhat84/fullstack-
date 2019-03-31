2ea7f8dfdd4a4034615e77ff40cd057ad0ec951f
Prashant Bhat <prashantbhat84@gmail.com>
2019-03-31 19:11:46 +0530

    engines and start script added in package json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({hi:'there'});
+  res.send({ hi: "there" });
 });
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);

8f938940c44c5aa255220bbee9d8b4c9218059b4
Prashant Bhat <prashantbhat84@gmail.com>
2019-03-31 18:57:02 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({hi:'there'});
+});
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
